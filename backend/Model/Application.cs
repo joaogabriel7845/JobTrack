6416c9095045022c524ebebe5966707c72e4a687
UserNoo <victorsansilva@gmail.com>
2026-06-19 17:25:50 -0300

    feat: configura cors para o react e conexao postgres

--- backend/Model/Application.cs
+++ backend/Model/Application.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
 public class Application
-{
+{   
+    [Key]
     public int Id { get; set; }
 
+    [MaxLength(30)]
+    [Required]
     public string Empresa { get; set; } = string.Empty;
+    [MaxLength(30)]
+    [Required]
     public string Cargo { get; set; } = string.Empty;
+    [MaxLength(30)]
+    [Required]
     public string Status { get; set; } = string.Empty;
 }
