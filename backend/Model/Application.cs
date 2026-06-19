c2f58243e8770acf3b297e5797535520e8840b4e
UserNoo <victorsansilva@gmail.com>
2026-06-19 13:41:15 -0300

    feat: primeira migration

---
+++ backend/Model/Application.cs
@@ -0,0 +1,8 @@
+public class Application
+{
+    public int Id { get; set; }
+
+    public string Empresa { get; set; } = string.Empty;
+    public string Cargo { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+}
